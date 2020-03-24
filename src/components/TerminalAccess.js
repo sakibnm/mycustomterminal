e8fb996c808c552a5a4d561d8acc1da3b08b031e
mmiazi <sakibnm@gmail.com>
2020-03-24 17:55:41 -0400

    Initial commit... empty

--- src/components/TerminalAccess.js
+++ src/components/TerminalAccess.js
@@ -1,7 +1,12 @@
 import React, { Component } from 'react';
-import {Tabs, Tab} from 'material-ui/Tabs';
+import { Tabs, Tab } from 'material-ui/Tabs';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import Terminal from 'react-bash';
+import request from 'superagent';
+import Dialog from 'material-ui/Dialog';
+import FlatButton from 'material-ui/FlatButton';
+import {Table, TableBody, TableHeader, TableHeaderColumn, TableRow, TableRowColumn} from 'material-ui/Table';
+var apiBaseUrl = "http://localhost:5000/api/";
 const history = [
     { value: 'Type `help` to begin' },
 ];
@@ -13,6 +18,20 @@ const structure = {
     },
     'README.md': { content: 'Some readme' },
 };
+let getData=function(input,callback){
+    request
+        .post(apiBaseUrl+'commandrun')
+        .send({ command: input})
+        .set('Accept', 'application/json')
+        .end(function(err, res){
+            // Calling the end function will send the request
+            if(err){
+                callback(err)
+            }
+            callback(res);
+        });
+};
+
 export default class TerminalAccess extends Component {
     constructor(props){
         super(props);
@@ -24,24 +43,123 @@ export default class TerminalAccess extends Component {
         const extensions = {
             sudo: {
                 exec: ({ structure, history, cwd }) => {
-                    return { structure, cwd,
+                    return {
+                        structure, cwd,
                         history: history.concat({ value: 'Nice try...' }),
                     };
                 },
             },
+            docker: {
+                exec: ({ structure, history, cwd }, command) => {
+                    let fetchData = getData(command.input, (res) => {
+                        var response = JSON.parse(res.text).result.split("\n");
+                        var tablerowData = response[1].split("   ");
+                        tablerowData = tablerowData.filter(function (item) {
+                            return item.length > 3;
+                        });
+                        var tableheaderData = response[0].split("   ");
+                        tableheaderData = tableheaderData.filter(function (item) {
+                            return item.length > 3;
+                        });
+                        var table = [], tableHeaderColumn = [], tableHeader = [], tableRowColumn = [], tableBody = [];
+                        tableheaderData.map(function (item) {
+                            tableHeaderColumn.push(
+                                <TableHeaderColumn>
+                                    {item}
+                                </TableHeaderColumn>
+                            );
+                            return item;
+                        });
+
+                        tableHeader.push(
+                            <TableHeader>
+                                <TableRow>
+                                    {tableHeaderColumn}
+                                </TableRow>
+                            </TableHeader>
+                        );
+
+                        tablerowData.map(function (item) {
+                            tableRowColumn.push(
+                                <TableRowColumn>
+                                    {item}
+                                </TableRowColumn>
+                            )
+                            return item;
+                        });
+
+                        tableBody.push(
+                            <TableBody>
+                                <TableRow>
+                                    {tableRowColumn}
+                                </TableRow>
+                            </TableBody>
+                        );
+                        table.push(
+                            <Table>
+                                {tableHeader}
+                                {tableBody}
+                            </Table>
+                        );
+                        this.setState({ commandData: table })
+                    });
+                    return { structure, cwd, history: history.concat({ value: 'Nice try...' }), };
+                },
+            },
+            portainer: {
+                exec: ({ structure, history, cwd }, command) => {
+                    let fetchData = getData("docker-machine ip manager1", (res) => {
+                        var response = JSON.parse(res.text).result.split("\n")[0];
+                        let embedUrl = "http://" + response + ":9000/"
+                        console.log("embedUrl", embedUrl)
+                        let applicationState = [];
+
+                        this.setState({ applicationState, dialogOpen:true })
+                    });
+                    return { structure, cwd, history: history.concat({ value: 'Nice try...' }), };
+                }
+            }
         };
-        return(
+        const actions = [
+
+            <FlatButton
+                label="Close"
+                primary={true}
+                keyboardFocused={true}
+                onTouchTap={(event) => this.handleClose(event)}
+            />,
+        ];
+
+        return (
             <MuiThemeProvider>
-                    <div className="parentContainer">
-                        <h4>Try your BASH commands here ;)</h4>
-                        <div style={{flex:1}}>
-                            <Terminal history={history} structure={structure} extensions={extensions} prefix={"user@HOMEPC"}/>
+                <Tabs>
+                    <Tab label="Docker Administration" >
+                        <div className="parentContainer">
+                            <center>
+                                <h4>Run docker administration commands here</h4>
+                                <div style={{ flex: 1 }}>
+                                    <Terminal history={history} structure={structure} extensions={extensions} prefix={"user@nsakib.xyz"} />
+                                </div>
+                            </center>
+                            <div>
+                                {this.state.commandData}
+                                <MuiThemeProvider>
+                                    <Dialog
+                                        title="Application Launcher"
+                                        actions={actions}
+                                        modal={true}
+                                        contentClassName="customDialog2"
+                                        open={this.state.dialogOpen}
+                                        onRequestClose={(event)=>this.handleClose(event)}
+                                    >
+                                        {this.state.applicationState}
+                                    </Dialog>
+                                </MuiThemeProvider>
+                            </div>
                         </div>
-                        <div>
-                            {this.state.commandData}
-                        </div>
-                    </div>
+                    </Tab>
+                </Tabs>
             </MuiThemeProvider>
-        );
+        )
     }
 }
